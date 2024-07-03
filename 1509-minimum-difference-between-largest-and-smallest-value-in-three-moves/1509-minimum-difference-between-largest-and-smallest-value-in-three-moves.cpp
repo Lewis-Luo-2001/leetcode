0e02b910f1f8fd425289dda9f096b95c660f89d0
Lewis Luo <60177489+Lewis-Luo-2001@users.noreply.github.com>
2024-07-03 18:06:13 +0800

    Time: 67 ms (66.75%), Space: 38.8 MB (50.84%) - LeetHub

--- 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
+++ 1509-minimum-difference-between-largest-and-smallest-value-in-three-moves/1509-minimum-difference-between-largest-and-smallest-value-in-three-moves.cpp
@@ -1,23 +1,12 @@
 class Solution {
 public:
     int minDifference(vector<int>& nums) {
-        if(nums.size() <= 3) return 0;
+        const int N = nums.size();
+
+        if(N <= 3) return 0;
 
         sort(nums.begin(), nums.end());
 
-        int left = 0, right = nums.size() - 1, count = 3;
-
-        while(left < right && count > 0) {
-            if(nums[left + 1] - nums[left] >= nums[right] - nums[right - 1]) {
-                left++;
-            }
-            else {
-                right--;
-            }
-
-            count--;
-        }
-
-        return nums[right] - nums[left];
+        return min({nums[N - 4] - nums[0], nums[N - 3] - nums[1], nums[N - 2] - nums[2], nums[N - 1] - nums[3]});
     }
 };
